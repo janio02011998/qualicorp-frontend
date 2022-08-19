153987c4b65baef18fbea28177ca7ccfe8edbfa7
JANIO <janio.carvalho@pixter.com.br>
2022-08-19 16:54:38 -0300

    UPDATE: add simple methods and types

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -7,7 +7,7 @@
             </v-list-item-action>
 
             <v-list-item-content>
-              <v-list-item-title>Notifications</v-list-item-title>
+              <v-list-item-title>{{task.title}}</v-list-item-title>
             </v-list-item-content>
           </template>
         </v-list-item>
@@ -16,7 +16,7 @@
 
 <script>
 export default {
-
+  props: ['task']
 }
 </script>
 
--- src/views/TaskView.vue
+++ src/views/TaskView.vue
@@ -1,21 +1,53 @@
 <template>
-  <v-list-item-group
-        v-model="settings"
-        multiple
-        active-class=""
-      >
-       <Task />
-       <Task />
+  <div>
+    <v-col cols="12" sm="6">
+      <v-text-field
+        v-model="inputField"
+        label="Tarefa diária:"
+        outlined
+        clearable
+        @keyup.enter="handleAddTask"
+      ></v-text-field>
+    </v-col>
+    <v-list flat subheader>
+      <v-list-item-group multiple active-class="">
+        <div v-for="(task, index) in tasks" :key="index">
+          <Task :task="task" />
+        </div>
       </v-list-item-group>
+    </v-list>
+  </div>
 </template>
 
 <script lang="ts">
-import Task from "@/components/Task.vue";   
-   
-  export default {
-    name: "Tarefas",
-    components: {
-      Task
+import Task from "@/components/Task.vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Tarefas",
+  components: {
+    Task,
+  },
+  data: () => ({
+    inputField: null,
+    tasks: [
+      { title: "Ir ao mercado", completed: false },
+      { title: "Fazer lanche", completed: false },
+      { title: "Malhar", completed: false },
+      { title: "Buscar filha", completed: false },
+      { title: "Ir ao jogo", completed: false },
+    ],
+  }),
+  methods:  {
+    handleAddTask() {
+      if(this.inputField) {
+        this.tasks.push({
+          title: this.inputField,
+          completed: false,
+        })
+        this.inputField = null;
+      }
     }
   }
+});
 </script>
